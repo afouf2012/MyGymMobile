02055a8561ec2a4883f756349c22efd052517e06
afouf2012 <49999844+afouf2012@users.noreply.github.com>
2021-11-07 16:41:44 +0100

    finalizing crud

---
+++ app/src/main/java/esprit/tn/AddExercices.java
@@ -0,0 +1,35 @@
+package esprit.tn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddExercices extends AppCompatActivity {
+
+    EditText title_input, categoriy_input, duration_input;
+    Button add_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_exercices);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        title_input = findViewById(R.id.title_input);
+        categoriy_input = findViewById(R.id.categoriy_input);
+        duration_input = findViewById(R.id.duration_input);
+        add_button = findViewById(R.id.add_button);
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyExercicesDB myDB = new MyExercicesDB(AddExercices.this);
+                myDB.addBook(title_input.getText().toString().trim(),
+                        categoriy_input.getText().toString().trim(),
+                        Integer.valueOf(duration_input.getText().toString().trim()));
+            }
+        });
+    }
+}
